38d15785ed865dbe1795e0d763c21ba0b11c8faf
markmin <realminthumon@gmail.com>
2019-12-06 21:04:52 +0800

    changes to quick_picks program

--- Practical 4/quick_picks.py
+++ Practical 4/quick_picks.py
@@ -20,9 +20,7 @@ def main():
                 number = random.randint(MINIMUM, MAXIMUM)
             quick_pick.append(number)
         quick_pick.sort()
-        # the following uses a generator expression (like a list comprehension)
-        # to format each number in quick_pick in the same way
-        # this is then turned into a single string with the join method
+
         print(" ".join("{:2}".format(number) for number in quick_pick))
 
 
